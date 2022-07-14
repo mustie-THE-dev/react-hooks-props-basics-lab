0958aa15f7332df8160e439ba6edbe09b1211d24
Simon kamau <simon.kamau@student.moringaschool.com>
2022-07-14 17:08:48 +0300

    added a file link.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Links from "./Links";
 
 function About() {
   return (
@@ -6,7 +7,9 @@ function About() {
       <h2>About Me</h2>
       <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
